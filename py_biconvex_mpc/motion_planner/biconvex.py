ba2fb013e99958e5492caeba63a1e385a667510b
Avadesh Meduri <avadesh02@gmail.com>
2020-12-10 01:39:27 -0500

    finished putting things together in the sovler. need to debug. Packaging complete

---
+++ py_biconvex_mpc/motion_planner/biconvex.py
@@ -0,0 +1,230 @@
+# This file contains the biconvex motion planner
+# Author : Avadesh Meduri  
+# Date : 6/12/2020
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from .. dynamics.centroidal import CentroidalDynamics
+from ..solvers.fista import FISTA
+
+class BiConvexMP(CentroidalDynamics):
+
+    def __init__(self, m, dt, T, n_eff, rho = 1e+1, L0 = 0.001, beta = 1.1, maxit = 500, tol = 1e-3):
+        '''
+        This is the Bi Convex motion planner
+        Input:
+            m : mass of the robot
+            dt : discretization of the dynamics
+            n_eff : number of end effectors
+            T : horizon of plan 
+            rho : penalty on dynamic constraint violation
+            L0 : starting step length size during line search
+            beta : increas factor of step length
+            maxit : maximum number of iterations each component of the biconvex optimization is run
+            tol : exit criteria of the optimization
+        '''
+        
+        self.dt = dt
+        self.m = m
+        self.n_col = int(np.round(T/self.dt))
+        self.n_eff = n_eff
+        self.rho = rho
+        self.maxit = maxit
+        self.tol = tol
+
+        self.cnt_arr = []
+        self.r_arr = []
+        # Note : no of collocation points should be computed only once
+        # and passed to centroidal dynamics
+        super().__init__(m, dt, T, n_eff)
+        # make the inputs to these parameters that come from outside
+        self.fista = FISTA(L0, beta)
+
+        # arrays to store statistics
+        self.f_all = [] # history of cost of force optimization
+        self.x_all = [] # history of cost of state optimization
+        self.total_all = [] # history of total cost
+
+    def create_contact_array(self, cnt_plan):
+        '''
+        This function creates the contact array [[1/0, 1/0], [..], ...]
+        and the contact location array 
+        Input:
+            cnt_plan : contact plan [1/0, x, y, z, start, end]
+        '''
+        assert np.shape(cnt_plan)[1] == self.n_eff
+        assert np.shape(cnt_plan)[2] == 6
+
+        cnt_arr = np.zeros((self.n_col, self.n_eff))
+        r_arr = np.zeros((self.n_col, self.n_eff, 3))
+
+        t_arr = np.zeros(self.n_eff)
+        for i in range(len(cnt_plan)):
+            for n in range(self.n_eff):
+                time_steps = int(np.round((cnt_plan[i][n][5] - cnt_plan[i][n][4])/self.dt, 2))
+                cnt_arr[:,n][int(t_arr[n]):int(t_arr[n]+time_steps)] = cnt_plan[i][n][0]
+                r_arr[int(t_arr[n]):int(t_arr[n]+time_steps), n] = np.tile(cnt_plan[i][n][1:4], (time_steps,1))
+                t_arr[n] += time_steps
+
+        self.cnt_arr = cnt_arr
+        self.r_arr = r_arr
+
+    def create_cost_X(self, W_X, W_X_ter, X_ter):
+        '''
+        Creates the cost matrix Q_X and q_X for the X optimization
+        Input:
+            W_X : weights on the tracking X
+            W_X_ter : terminal weights on the tracking of X
+        '''
+        assert len(W_X) == len(W_X_ter)
+        assert len(W_X) == 9
+
+        self.Q_X = np.zeros((9*(self.n_col + 1), 9*(self.n_col + 1)))    
+        np.fill_diagonal(self.Q_X, W_X)
+        np.fill_diagonal(self.Q_X[-9:, -9:], W_X_ter)
+        # self.q = np.repeat(W_X, self.n_col + 1)
+        self.q_X = np.zeros(((self.n_col+1)*9,1))
+        self.q_X[-9:,0] = -2*W_X_ter*X_ter
+
+        self.Q_X = np.matrix(self.Q_X)
+        self.q_X = np.matrix(self.q_X)
+
+    def create_cost_F(self, W_F):
+        '''
+        Creates the cost matrix Q_F and q_F for the F optimization
+        Input:
+            W_F : weights on the F
+        '''
+        assert len(W_F) == 3*self.n_eff
+
+        self.Q_F = np.zeros((self.n_col*3*self.n_eff, self.n_col*3*self.n_eff))
+        np.fill_diagonal(self.Q_F, W_F)
+        self.q_F = np.zeros((self.n_col*3*self.n_eff, 1))
+
+        self.Q_F = np.matrix(self.Q_F)
+        self.q_F = np.matrix(self.q_F)
+
+
+    def optimize(self, X_init, X_ter, W_X, W_F, W_X_ter, no_iters, X_wm = None, F_wm = None):
+        '''
+        This function optimizes the centroidal dynamics trajectory
+        Input:
+            X_init : initial state vector (com(x,y,z), com_vel(x,y,z), AMOM(x,y,z))
+            X_ter : desired terminal state vector at the end of the trajectory
+            W_X : running weights on the X optimization
+            W_F : running weights on the F optimization
+            W_X_ter : terminal weights on the X optimization
+            no_iters : number of iterations between x and f
+            X_wm : starting X to warm start F optimization
+            F_wm : starting F to warm start X optimization
+        '''
+        # creating cost matrices
+        self.create_cost_X(W_X, W_X_ter, X_ter)
+        self.create_cost_F(W_F)
+
+        if X_wm:
+            X_k = X_wm
+        else:
+            X_k = np.ones((9*(self.n_col+1),1))
+
+        if F_wm:
+            F_k = F_wm
+        else:
+            F_k = np.zeros((3*self.n_col*self.n_eff,1))
+            F_k[2::3,0] = self.m*9.81
+
+        # penalty of dyannic constraint violation from ADMM
+        P_k = np.zeros((9*self.n_col + 9, 1))
+
+        for k in range(no_iters):
+            print("iter number {}".format(k), end='\n')
+            self.fista.reset()
+            # optimizing for f
+            A_x, b_x = self.compute_X_mat(X_k, self.r_arr, self.cnt_arr)
+            obj_f = lambda f :f.T *self.Q_F*f + self.q_F.T*f + self.rho*np.linalg.norm(A_x*f - b_x + P_k)**2    
+            # gradient of the objective function that optimizes for f 
+            grad_obj_f = lambda f: 2*self.Q_F*f + self.q_F + 2*self.rho*A_x.T*(A_x*f - b_x + P_k)
+            # projection of f into constraint space (friction cone and max f)
+            proj_f = lambda f, L : f
+            F_k_1 = self.fista.optimize(obj_f, grad_obj_f, proj_f, F_k, self.maxit, self.tol)
+
+            self.fista.reset()
+            # optimizing for x
+            A_f, b_f = self.compute_F_mat(F_k_1, self.r_arr, self.cnt_arr, X_init)
+            obj_x = lambda x :x.T *self.Q_X*x + self.q_X.T*x + self.rho*np.linalg.norm(A_f*x - b_f + P_k)**2    
+            # gradient of the objective function that optimizes for f 
+            grad_obj_x = lambda x: 2*self.Q_X*x + self.q_X + 2*self.rho*A_f.T*(A_f*x - b_f + P_k)
+            # projection of f into constraint space (friction cone and max f)
+            proj_x = lambda x, L : x
+            X_k_1 = self.fista.optimize(obj_x, grad_obj_x, proj_x, X_k, self.maxit, self.tol)
+
+            # update of P_k
+            P_k_1 = P_k + np.abs(A_f*X_k_1 - b_f)
+
+            # preparing for next iteration
+            X_k = X_k_1
+            F_k = F_k_1
+            P_k = P_k_1
+            
+            # computing cost of total optimization problem
+            dyn_violation = self.rho*np.linalg.norm(A_f*X_k - b_f)**2
+            cost_x = X_k.T *self.Q_X*X_k + self.q_X.T*X_k + dyn_violation
+            cost_f = F_k.T *self.Q_F*F_k + self.q_F.T*F_k + dyn_violation
+            total_cost = cost_x + cost_f - dyn_violation
+            self.f_all.append(float(cost_x))            
+            self.x_all.append(float(cost_f))            
+            self.total_all.append(float(total_cost))            
+
+            # if k > 2 and np.abs(self.f_all[-2] - self.f_all[-1]) < self.tol:
+            #     break
+        self.X_opt = X_k
+        self.F_opt = F_k
+        
+        return X_k, F_k
+
+    def stats(self):
+        print("solver terminated in {} iterations", len(self.f_all))
+
+        fig, ax = plt.subplots(3,1)
+        ax[0].plot(self.X_opt[0::9], label = "Cx")
+        ax[0].plot(self.X_opt[1::9], label = "Cy")
+        ax[0].plot(self.X_opt[2::9], label = "Cz")
+        ax[0].grid()
+        ax[0].legend()
+
+        ax[1].plot(self.X_opt[3::9], label = "Vx")
+        ax[1].plot(self.X_opt[4::9], label = "Vy")
+        ax[1].plot(self.X_opt[5::9], label = "Vz")
+        ax[1].grid()
+        ax[1].legend()
+
+        ax[2].plot(self.X_opt[6::9], label = "ang_x")
+        ax[2].plot(self.X_opt[7::9], label = "ang_y")
+        ax[2].plot(self.X_opt[8::9], label = "ang_z")
+        ax[2].grid()
+        ax[2].legend()
+
+
+        fig, ax_f = plt.subplots(self.n_eff,1)
+        for n in range(self.n_eff):
+            ax_f[n].plot(self.F_opt[3*n::3], label = "ee: " + str(n) + "Fx")
+            ax_f[n].plot(self.F_opt[3*n+1::3], label = "ee: " + str(n) + "Fy")
+            ax_f[n].plot(self.F_opt[3*n+2::3], label = "ee: " + str(n) + "Fz")
+            ax_f[n].grid()
+            ax_f[n].legend()
+
+        fig, ax_cost = plt.subplots(3,1)
+        ax_cost[0].plot(self.x_all, label = "cost_x")
+        ax_cost[0].grid()
+        ax_cost[0].legend()
+
+        ax_cost[1].plot(self.f_all, label = "cost_f")
+        ax_cost[1].grid()
+        ax_cost[1].legend()
+
+        ax_cost[2].plot(self.total_all, label = "cost_total")
+        ax_cost[2].grid()
+        ax_cost[2].legend()
+
+        plt.show()
---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from os import path, walk
+from setuptools import setup, find_packages
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'biconvex_mpc'))
+
+def print_error(*args, **kwargs):
+    """ Print in stderr. """
+    print(*args, file=sys.stderr, **kwargs)
+
+# Package name.
+package_name = "biconvex_mpc"
+
+# Long description from the readme.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+# Install the package.xml.
+data_files_to_install = [(path.join("share", package_name), ["package.xml"])]
+
+# Install nodes and demos.
+scripts_list = []
+for (root, _, files) in walk(path.join("demos")):
+    for demo_file in files:
+        scripts_list.append(path.join(root, demo_file))
+
+# Final setup.
+setup(
+    name=package_name,
+    version="1.0.0",
+    package_dir={package_name: package_name},
+    packages=[package for package in find_packages()
+                if package.startswith('py_biconvex_mpc')],
+    data_files=data_files_to_install,
+    scripts=scripts_list,
+    install_requires=[
+          'numpy>=1.10.4',
+      ],
+    zip_safe=True,
+    maintainer="ameduri",
+    maintainer_email="am9789@nyu.edu",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    description="This repo contains the centroidal motion planner based on a Biconvex solver",
+    license="BSD-3-clause",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD-3-clause",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

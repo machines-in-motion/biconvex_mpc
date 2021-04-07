fe97ef72c6073818f593b538fe6072079b1cc565
Neotriple <neotriple@gmail.com>
2021-04-07 06:43:06 -0700

    Fixed up a bit of the PyBind functionality...

--- srcpy/solvers/fista.cpp
+++ srcpy/solvers/fista.cpp
@@ -1,9 +1,7 @@
 // This file contains python bindings for fista
-
 #include <fista.hpp>
 
 #include <pybind11/pybind11.h>
-#include <eigen3/Eigen/Dense>
 
 using namespace solvers;
 using namespace function;
@@ -14,13 +12,13 @@ PYBIND11_MODULE(fista_py, m)
     m.doc() = "FISTA (first order shrinkage method)";
 
     //FISTA
-    py::class_<FISTA> fista(m, "Fista instance");
-    fista.def(py::init<double,double,double>());
-    fista.def("optimize", &solvers::FISTA::optimize);
+    py::class_<solvers::FISTA> fista(m, "Fista instance");
+    fista.def(py::init<double, double, double>());
+    //fista.def("optimize", &solvers::FISTA::optimize);
 
     //Problem
-    py::class_<ProblemData> problem_data(m, "Problem Data");
-    problem_data.def(py::init<Eigen::MatrixXd, Eigen::VectorXd, Eigen::MatrixXd, Eigen::VectorXd>());
-    problem_data.def("compute_obj", &function::ProblemData::compute_obj);
+//    py::class_<ProblemData> problem_data(m, "Problem Data");
+//    problem_data.def(py::init<Eigen::MatrixXd, Eigen::VectorXd, Eigen::MatrixXd, Eigen::VectorXd>());
+//    problem_data.def("compute_obj", &function::ProblemData::compute_obj);
 }
 

85de3aeeb26e6d93c7fd2417b91467f38150ed71
Neotriple <neotriple@gmail.com>
2021-04-07 13:07:46 -0700

    Changed default behavior of prob_data pybind functionality to use the pybind function

--- srcpy/solvers/fista.cpp
+++ srcpy/solvers/fista.cpp
@@ -24,6 +24,6 @@ PYBIND11_MODULE(fista_py, m)
     //Problem
      py::class_<ProblemData> problem_data(m, "data");
      problem_data.def(py::init<Eigen::MatrixXd, Eigen::VectorXd, Eigen::MatrixXd, Eigen::VectorXd>());
-     problem_data.def("compute_obj", &function::ProblemData::compute_obj);
+     problem_data.def("compute_obj", &function::ProblemData::compute_obj_pybind);
 }
 

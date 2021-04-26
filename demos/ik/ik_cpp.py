e0fba8a23c0a22b0456f2fbd942f39fcb7b939a0
Neotriple <neotriple@gmail.com>
2021-04-26 16:28:08 -0400

    Added test_ik cpp file to test C++ IK

--- demos/ik/ik_cpp.py
+++ demos/ik/ik_cpp.py
@@ -14,4 +14,4 @@ T = 1.0
 des_pos = np.array([0,0,0])
 
 ik = InverseKinematics(Solo12Config.urdf_path, dt, T)
-ik.add_position_tracking_task(robot.model.getFrameId("FL_FOOT"), 0.0, 0.5, des_pos, 1e2, "FL_pos_task")
+#ik.add_position_tracking_task(robot.model.getFrameId("FL_FOOT"), 0.0, 0.5, des_pos, 1e2, "FL_pos_task")

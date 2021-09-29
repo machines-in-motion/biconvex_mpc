1825ede36bd2d355cea1759f4693c1943478f01e
Neotriple <neotriple@gmail.com>
2021-09-29 02:12:44 +0100

    Updated acyclic contact planner, working on demo

--- contact_planner/acyclic_contact_generator.py
+++ contact_planner/acyclic_contact_generator.py
@@ -1,73 +1,67 @@
- import pinocchio as pin
+import pinocchio as pin
+import numpy as np
 
- from gait_planner_cpp import GaitPlanner
-
- class ContactPlanner:
-
+class ContactPlanner:
     def __init__(self, acyclic_plan, robot_info, height_map = None):
-        self.gait_period = gait_params.gait_period
-        self.stance_percent = gait_params.stance_percent
-        self.nominal_height = gait_params.nom_ht
-        self.phase_offset = gait_params.phase_offset
-        self.step_height = gait_params.step_height
-
         self.foot_size = robot_info.foot_size
         self.gravity = 9.81
+        self.n_eef = robot_info.num_eef
 
         #Read this in from somewhere
-        #TODO: 
+        #TODO:
         self.dt = 0.005
-        
+
         #Create instance of continuous gait planner
-        self.acyclic_contact_plan = acyclic_plan
-    def create_cnt_plan(self, q, v, t):
+        self.acyclic_contact_plan = np.array(acyclic_plan)
+
+        #Use current contact location when computing locations
+        self.use_current_eef_location = True
+        self.use_current_contact = False
+
+    def create_cnt_plan(self, q, v, t, current_contact, eef_locations):
             """
             Creates Contact Plan Matrix:
+            Input: q (position), v (velocity), t (current time), current_contact (boolean array of which end-effectors
+                are in contact), eef_locations (n_eef x 3 matrix of end-effector positions)
+            Output: Contact Plan Matrix
             Dimensions: horizon (number of collocation points) x num_eef x 9
             Extra Information: The final dimension (9) gives the contact boolean
-                               i.e. the last vector should be [1/0, pos, orientation (quaternion), dt] 
+                               i.e. the last vector should be [1/0, pos, orientation (quaternion), dt]
                                where 1/0 gives a boolean for contact (1 = contact, 0 = no cnt)
             """
             cnt_plan = np.zeros((self.horizon, len(self.eff_names), 9))
             evaluated_time = t
 
-            #TODO: Read this from a height-map?
+            #TODO: Read this from a height-map
             temp_quat = np.array([0,0,0,1])
-            
+            prev_current_eef_used = np.ones(self.n_eef)
+
             for i in range(self.horizon):
                 evaluated_time += i*self.dt
 
                 for j in range(len(self.eff_names)):
-                    for k in range(self.acyclic_contact_plan[j].shape[0]):
-                        if k == 0:
-                            if self.evaluated_time <= self.acyclic_contact_plan[j][0][3] \
-                                and self.evaluated_time >= self.acyclic_contact_plan[j][0][4]:
-                                cnt_plan[i][j][0] = 1
-                                cnt_plan[i][j][1:4] = self.acyclic_contact_plan[j][0][0:3]
-                                cnt_plan[i][j][4:8] = temp_quat
-                                cnt_plan[i][j][8] = self.dt
-                        elif k == self.acyclic_contact_plan[j].shape[0]:
-                            if self.evaluated_time <= self.acyclic_contact_plan[j][k][3] \
-                                and self.evaluated_time >= self.acyclic_contact_plan[j][k][4]:
-                                cnt_plan[i][j][0] = 1
-                                cnt_plan[i][j][1:4] = self.acyclic_contact_plan[j][k][0:3]
-                                cnt_plan[i][j][4:8] = temp_quat
-                                cnt_plan[i][j][8] = self.dt
-                            if self.evaluated_time >= self.acyclic_contact_plan[j][k][3]:
-                                cnt_plan[i][j][0] = 1
-                                cnt_plan[i][j][1:4] = self.acyclic_contact_plan[j][k][0:3]
-                                cnt_plan[i][j][4:8] = temp_quat
-                                cnt_plan[i][j][8] = self.dt
-                        else:
-                            if self.evaluated_time <= self.acyclic_contact_plan[j][k][3] \
-                                and self.evaluated_time >= self.acyclic_contact_plan[j][k][4]:
-                                cnt_plan[i][j][0] = 1
-                                cnt_plan[i][j][1:4] = self.acyclic_contact_plan[j][k][0:3]
-                                cnt_plan[i][j][4:8] = temp_quat
-                                cnt_plan[i][j][8] = self.dt
-                            if self.evaluated_time >= self.acyclic_contact_plan[j][k][3]\
-                                and self.evaluated_time <= self.acyclic_contact_plan[j][k+1][4]:
-                                cnt_plan[i][j][0] = 0
-                    
+                    for k in range(self.acyclic_contact_plan.shape[0]):
+                        if self.acyclic_contact_plan[k][j][4] >= self.evaluated_time \
+                                > self.acyclic_contact_plan[k][j][5]:
+                            cnt_plan[i][j][0] = self.acyclic_contact_plan[k][j][0]
+                            cnt_plan[i][j][1:4] = self.acyclic_contact_plan[k][j][1:4]
+                            cnt_plan[i][j][4:8] = temp_quat
+                            cnt_plan[i][j][8] = self.dt
 
-            return cnt_plan
+                            if self.use_current_eef_location and cnt_plan[i][j][0] == 1:
+                                if prev_current_eef_used[j] == 1:
+                                    cnt_plan[i][j][1:4] = eef_locations[j]
+
+                            if cnt_plan[i][j][0] == 0:
+                                prev_current_eef_used[j] = 0
+                        else:
+                            cnt_plan[i][j][0] = 1
+                            cnt_plan[i][j][1:4] = self.acyclic_contact_plan[k][j][1:4]
+                            cnt_plan[i][j][4:8] = temp_quat
+                            cnt_plan[i][j][8] = self.dt
+
+                            if self.use_current_eef_location and cnt_plan[i][j][0] == 1:
+                                if prev_current_eef_used[j] == 1:
+                                    cnt_plan[i][j][1:4] = eef_locations[j]
+
+                return cnt_plan

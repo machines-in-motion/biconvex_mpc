Changes to rearing, trying to debug swing leg IK costs, fixed CoM computation in plotting

demos/mpc/abstract_acyclic_gen.py
@@ -126,8 +126,6 @@ class SoloAcyclicGen:
         X_init = np.zeros(9)
         pin.computeCentroidalMomentum(self.rmodel, self.rdata)
         X_init[0:3] = pin.centerOfMass(self.rmodel, self.rdata, q.copy(), v.copy())
-        print("Center of mass: ")
-        print(X_init[0:3])
         X_init[3:] = np.array(self.rdata.hg)
         X_init[3:6] /= self.m
 
@@ -186,6 +184,7 @@ class SoloAcyclicGen:
                                 if self.params.swing_wt[k][j][0] > 0:
                                     self.ik.add_position_tracking_task_single(self.ee_frame_id[j], self.params.swing_wt[k][j][1:4], self.params.swing_wt[k][j][0],
                                                                 "swing_" + str(0) + self.eff_names[j], i)
+                                    print("Adding Swing Cost")
                             break
                 else:
                     if not make_cyclic:
@@ -324,17 +323,16 @@ class SoloAcyclicGen:
         F_opt = self.mp.return_opt_f()
         momentum_opt = self.mp.return_opt_mom()
 
+        com = pin.centerOfMass(self.rmodel, self.rdata, q.copy(), v.copy())
+
         # Plot Center of Mass
         fig, ax = plt.subplots(3,1)
         ax[0].plot(com_opt[:, 0], label="com x")
-        ax[0].plot(q[0], 'o', label="Currente Center of Mass x")
+        ax[0].plot(com[0], 'o', label="Current Center of Mass x")
         ax[1].plot(com_opt[:, 1], label="com y")
-        ax[1].plot(q[1], 'o', label="Currente Center of Mass y")
+        ax[1].plot(com[1], 'o', label="Current Center of Mass y")
         ax[2].plot(com_opt[:, 2], label="com z")
-        ax[2].plot(q[2], 'o', label="Current Center of Mass z")
-
-        print("Z_error: ")
-        print(abs(q[2]-com_opt[:, 2][0]))
+        ax[2].plot(com[2], 'o', label="Current Center of Mass z")
 
         ax[0].grid()
         ax[0].legend()

demos/mpc/motions/plan_jump.py
@@ -61,10 +61,10 @@ plan.X_ter = [0.2, 0, 0.22, 0, 0, 0, 0, 0.0, 0.0]
 plan.cent_wt = [1e1, 1e4]
 plan.cnt_wt = 1e3
 
-# plan.swing_wt = [[[1e2, 0.3946,   0.14695,  0.0, st + 0.25*flight_time, st + 0.5*flight_time],
-#                   [1e2, 0.3946,   -0.14695,  0.0, st + 0.25*flight_time, st + 0.5*flight_time],
-#                   [1e2,  0.0054,   0.14695,  0., st + 0.25*flight_time, st + 0.5*flight_time],
-#                   [1e2,  0.0054,   -0.14695,  0., st + 0.25*flight_time, st + 0.5*flight_time]]]
+plan.swing_wt = [[[1e2, 0.3946,   0.14695,  0.0, st + 0.25*flight_time, st + 0.5*flight_time],
+                  [1e2, 0.3946,   -0.14695,  0.0, st + 0.25*flight_time, st + 0.5*flight_time],
+                  [1e2,  0.0054,   0.14695,  0., st + 0.25*flight_time, st + 0.5*flight_time],
+                  [1e2,  0.0054,   -0.14695,  0., st + 0.25*flight_time, st + 0.5*flight_time]]]
 
 x_reg1 = np.concatenate([q0, pin.utils.zero(rmodel.nv)])
 

demos/mpc/motions/rearing.py
@@ -1,6 +1,6 @@
-## This file contains the motion plan for cartwheel
-## Author : Avadesh Meduri
-## Date : 21/09/2021
+## This file contains the motion plan for rearing
+## Author : Avadesh Meduri & Paarth Shah
+## Date : 04/10/2021
 
 import numpy as np
 from robot_properties_solo.config import Solo12Config
@@ -16,12 +16,13 @@ q0 = np.array(Solo12Config.initial_configuration)
 v0 = pin.utils.zero(rmodel.nv)
 x0 = np.concatenate([q0, pin.utils.zero(rmodel.nv)])
 
+# Quaternion Convention: x,y,z,w
 
 ###########################################################################################
 plan = ACyclicMotionParams("solo12", "rearing")
 
 st = 0.5
-rear_time = 0.3
+rear_time = 0.4
 T = 1.2
 dt = 0.05
 
@@ -35,8 +36,8 @@ plan.cnt_plan = [[[1.,      0.3946,   0.14695,  0., 0.,  st    ],
                   [1.,      0.0054,   0.14695,  0., st, st + rear_time],
                   [1.,      0.0054,  -0.14695,  0., st, st + rear_time]],
 
-                 [[1.,      0.3946,   0.14695,  0., st + rear_time, T],
-                  [1.,      0.3946,  -0.14695,  0., st + rear_time, T],
+                 [[1.,      0.41,   0.14695,  0., st + rear_time, T],
+                  [1.,      0.41,  -0.14695,  0., st + rear_time, T],
                   [1.,      0.0054,   0.14695,  0., st + rear_time, T],
                   [1.,      0.0054,  -0.14695,  0., st + rear_time, T]]]
 
@@ -49,26 +50,27 @@ plan.W_X_ter = 10*np.array([1e+3, 1e1, 1e+5, 1e-1, 1e-1, 1e-1, 1e+2, 1e+3, 1e+2]
 plan.W_F = np.array(4*[1e+1, 1e+1, 1e+0])
 plan.rho = 5e+4
 
-plan.X_nom = [[0.2, 0, 0.22, 0, 0, 0, 0, -0.0, 0.0, 0.0, st],
-              [0.18, 0, 0.28, 0, 0, 0, 0, 0.0, 0., st, st+rear_time],
+plan.X_nom = [[0.2, 0, 0.22, 0, 0, 0, 0, -0.05, 0.0, 0.0, st],
+              [0.18, 0, 0.28, 0, 0, 0, 0, -0.35, 0., st, st+rear_time],
               [0.2, 0, 0.22, 0, 0, 0, 0, 0.0, 0., st+rear_time, T]]
 
 plan.X_ter = [0.2, 0, 0.22, 0, 0, 0, 0, 0.0, 0.0]
 
 # ik optimization params
 
-plan.cent_wt = [1e3, 1e2] # CoM, Momentum
-plan.cnt_wt = 1e2 # End-Effector Contact Weight
+plan.cent_wt = [1e2, 1e2]  # CoM, Momentum
+plan.cnt_wt = 1e2  # End-Effector Contact Weight
 
-plan.swing_wt = [[[1e2, 0.3946,   0.14695,  0.1, st + 0.25*rear_time, st + 0.5*rear_time],
-                  [1e2, 0.3946,   -0.14695,  0.1, st + 0.25*rear_time, st + 0.5*rear_time],
+plan.swing_wt = [[[1e4, 0.4,   0.14695,  0.1, st + 0.25*rear_time, st + 0.5*rear_time],
+                  [1e4, 0.4,   -0.14695,  0.4, st + 0.25*rear_time, st + rear_time],
                   [0,  0.0054,   0.14695,  0., st + 0.25*rear_time, st + 0.5*rear_time],
                   [0,  0.0054,   -0.14695,  0., st + 0.25*rear_time, st + 0.5*rear_time]]]
 
 x_reg1 = np.concatenate([q0, pin.utils.zero(rmodel.nv)])
+x_reg2 = np.concatenate([q0, pin.utils.zero(rmodel.nv)])
 
-state_wt_1 = np.array([1e-2, 1e-2, 1e-2] + [0, 0, 0] + 2*[1e0, 1e0, 1e0] + 2*[1e1, 1e1, 1e1] +
-                      [0.00] * 3 + [0, 0, 0] + [0.1] *(rmodel.nv - 6))
+state_wt_1 = np.array([1e-2, 1e-2, 1e-2] + [0, 0, 1.0] + 2*[1e-3, 1e-3, 1e-3] + 2*[1e1, 1e1, 1e1] +
+                      3*[0.00] + [0, 0, 1.0] + (rmodel.nv - 6)*[0.5])
 
 plan.state_reg = [np.hstack((x_reg1, [0, T]))]
 plan.state_wt = [np.hstack((state_wt_1, [0, T]))]

demos/mpc/solo12_acyclic.py
@@ -14,7 +14,7 @@ from motions.plan_cartwheel import plan
 from motions.rearing import plan
 #from motions.plan_hifive import plan
 #from motions.stand import plan
-#from motions.plan_jump import plan
+from motions.plan_jump import plan
 
 pin_robot = Solo12Config.buildRobotWrapper()
 rmodel = pin_robot.model
@@ -41,7 +41,7 @@ mg.update_motion_params(plan, sim_t)
 
 time.sleep(2)
 
-plot_time = np.inf
+plot_time = 0.0
 
 for o in range(int(500*(plan_freq/sim_dt))):
 
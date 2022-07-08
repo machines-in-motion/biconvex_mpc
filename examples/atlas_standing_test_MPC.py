fixed normalizing m

examples/atlas_standing_test_MPC.py
@@ -31,14 +31,18 @@ n_eff = len(eff_names)
 
 q0 = np.array(AtlasConfig.initial_configuration)
 q0[0:2] = 0.0
-# q0[2] = 1.0
+# q0[10] = +np.pi/4
+# q0[11] = -np.pi/4
+# q0[17] = -np.pi/4
+# q0[18] = +np.pi/4
+
 v0 = pin.utils.zero(pin_robot.model.nv)
 x0 = np.concatenate([q0, pin.utils.zero(pin_robot.model.nv)])
 
 v_des = np.array([0.0,0.0,0.0])
 w_des = 0.0
 
-plan_freq = 0.1 # sec
+plan_freq = 0.05 # sec
 update_time = 0.0 # sec (time of lag)
 
 gait_params = still
@@ -59,20 +63,22 @@ index = 0
 pln_ctr = 0
 lag = 0
 
-for o in range(int(150*(plan_freq/sim_dt))):
+for o in range(int(550*(plan_freq/sim_dt))):
 
     # this bit has to be put in shared memory
     q, v = robot.get_state()
     
     if pln_ctr == 0:
+        print("time: ", o/1000)
         contact_configuration = len(eff_names)*[1,]
         pr_st = time.time()
         xs_plan, us_plan, f_plan = gg.optimize(q, v, np.round(sim_t,3), v_des, w_des)
         # Plot if necessary
         # if sim_t >= plot_time:
-        # gg.plot(q, v, plot_force=True)
+        # if o/1000 > 1:
+        #     gg.plot(q, v, plot_force=False)
             # gg.save_plan("trot")
-        print(q, v)
+
         pr_et = time.time()
         # solve_times.append(pr_et - pr_et)
 
@@ -98,7 +104,7 @@ for o in range(int(150*(plan_freq/sim_dt))):
     
     robot.send_joint_command(tau)
 
-    time.sleep(0.005)
+    time.sleep(0.001)
     sim_t += sim_dt
     pln_ctr = int((pln_ctr + 1)%(plan_freq/sim_dt))
     index += 1

examples/mpc/abstract_cyclic_gen1.py
@@ -280,17 +280,17 @@ class AbstractGaitGen:
         pin.computeCentroidalMomentum(self.rmodel, self.rdata)
         self.X_init[0:3] = pin.centerOfMass(self.rmodel, self.rdata, q.copy(), v.copy())
         self.X_init[3:] = np.array(self.rdata.hg)
-        self.X_init[3:6] /= self.m
+        self.X_init[3:] /= self.m
 
         self.X_nom[0::9] = self.X_init[0]
-        for i in range(1, self.horizon):
-            self.X_nom[9*i+0] = self.X_nom[9*(i-1)+0] + v_des[0]*self.dt_arr[i]
-            self.X_nom[9*i+1] = self.X_nom[9*(i-1)+1] + v_des[1]*self.dt_arr[i]
+        # for i in range(1, self.horizon):
+        #     self.X_nom[9*i+0] = -2*self.X_nom[9*(i-1)+0]
+            # self.X_nom[9*i+1] = -10*self.X_nom[9*(i-1)+1]
 
         self.X_nom[2::9] = self.params.nom_ht
-        self.X_nom[3::9] = v_des[0]
-        self.X_nom[4::9] = v_des[1]
-        self.X_nom[5::9] = v_des[2]
+        self.X_nom[3::9] = -0.01*self.X_init[3]
+        self.X_nom[4::9] = -0.01*self.X_init[4]
+        # self.X_nom[5::9] = -10*self.X_init[5]
 
         #Compute angular momentum / orientation correction
         R = pin.Quaternion(np.array(ori_des)).toRotationMatrix()
@@ -302,9 +302,9 @@ class AbstractGaitGen:
         amom = self.compute_ori_correction(q, des_quat.coeffs())
 
         #Set terminal references
-        X_ter[0:2] = self.X_init[0:2] + (self.params.gait_horizon*self.params.gait_period*v_des)[0:2] #Changed this
+        X_ter[0:2] = -0.1*self.X_init[0:2] 
         X_ter[2] = self.params.nom_ht
-        X_ter[3:6] = v_des
+        X_ter[3:5] = -0.1*self.X_init[3:5]
         X_ter[6:] = amom
 
         self.X_nom[6::9] = amom[0]*self.params.ori_correction[0]
@@ -365,7 +365,7 @@ class AbstractGaitGen:
         # pinocchio complains otherwise
         q = pin.normalize(self.rmodel, q)
         print("max iters")
-        self.kd.optimize(q, v, 50, 1)
+        self.kd.optimize(q, v, 30, 1)
         t3 = time.time()
 
         # print("Cost Time :", t2 - t1)